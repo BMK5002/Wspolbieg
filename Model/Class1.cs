af9b73a602265381e90ac28e08da295d2f542628
BMK5002 <tadzio2005@gmail.com>
2026-03-21 10:57:05 +0100

    Added class Calculator

--- Model/Class1.cs
+++ Model/Class1.cs
@@ -1,7 +1,47 @@
 ﻿namespace Model
 {
-    public class Class1
+    public class Calculator
     {
+        private int value1;
+        private int value2;
+        public Calculator(int value1, int value2)
+        {
+            this.value1 = value1;
+            this.value2 = value2;
+        }
 
+        public int add()
+        {
+            return value1 + value2;
+        }
+
+        public int substract()
+        {
+            return value1 - value2;
+        }
+
+        public int multiply()
+        {
+            return value1 * value2;
+        }
+
+        public int divide()
+        {
+            if (value2 == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+            return value1 / value2;
+        }
+
+        public void setValue1(int value)
+        {
+            value1 = value;
+        }
+
+        public void setValue2(int value)
+        { 
+            value2 = value;
+        }
     }
 }
