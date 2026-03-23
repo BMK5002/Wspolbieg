18e48ceec29432cc3652c3a5d61805a0300cc61c
BMK5002 <tadzio2005@gmail.com>
2026-03-23 21:20:37 +0100

    Added BallService class to Model

---
+++ Model/BallService.cs
@@ -0,0 +1,19 @@
+﻿using Data;
+
+namespace Model
+{
+    public class BallService : IBallService
+    {
+        public void Update(Ball Ball, double width, double height)
+        {
+            Ball.X += Ball.VelocityX;
+            Ball.Y += Ball.VelocityY;
+
+            if (Ball.X < 0 || Ball.X > width)
+                Ball.VelocityX *= -1;
+
+            if (Ball.Y < 0 || Ball.Y > height)
+                Ball.VelocityY *= -1;
+        }
+    }
+}
---
+++ Model/IBallService.cs
@@ -0,0 +1,9 @@
+﻿using Data;
+
+namespace Model
+{
+    public interface IBallService
+    {
+        void Update(Ball Ball, double width, double height);
+    }
+}
