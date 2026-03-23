d23bd68b9000fd07466f48d5ebf3a0dc8f7d6f57
BMK5002 <tadzio2005@gmail.com>
2026-03-23 21:19:32 +0100

    Added Ball class

---
+++ Data/Ball.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Data
+{
+    public class Ball : INotifyPropertyChanged
+    {
+        private double _x;
+        private double _y;
+
+        public double X
+        {
+            get => _x;
+            set
+            {
+                if (_x == value) return;
+                _x = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double Y
+        {
+            get => _y;
+            set
+            {
+                if (_y == value) return;
+                _y = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double VelocityX { get; set; }
+        public double VelocityY { get; set; }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
