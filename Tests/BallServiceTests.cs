e2022c1d2f31167aa17ed2cf2046f2238bc5f1ab
BMK5002 <tadzio2005@gmail.com>
2026-03-23 21:21:38 +0100

    Added tests

---
+++ Tests/BallServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using Data;
+using Model;
+
+public class BallServiceTests
+{
+    [Fact]
+    public void BallMoveTest()
+    {
+        var service = new BallService();
+        var Ball = new Ball { X = 10, Y = 10, VelocityX = 1, VelocityY = 1 };
+
+        service.Update(Ball, 100, 100);
+
+        Assert.Equal(11, Ball.X);
+        Assert.Equal(11, Ball.Y);
+    }
+}
---
+++ Tests/BallTests.cs
@@ -0,0 +1,37 @@
+﻿using Data;
+using Xunit;
+
+public class BallTests
+{
+    [Fact]
+    public void BallValuesTest()
+    {
+        var Ball = new Ball
+        {
+            X = 10,
+            Y = 20,
+            VelocityX = 1.5,
+            VelocityY = -2.5
+        };
+
+        Assert.Equal(10, Ball.X);
+        Assert.Equal(20, Ball.Y);
+        Assert.Equal(1.5, Ball.VelocityX);
+        Assert.Equal(-2.5, Ball.VelocityY);
+    }
+
+    [Fact]
+    public void BallSetterTest()
+    {
+        var Ball = new Ball();
+
+        Ball.X = 5;
+        Ball.Y = 6;
+
+        Ball.X = 15;
+        Ball.Y = 16;
+
+        Assert.Equal(15, Ball.X);
+        Assert.Equal(16, Ball.Y);
+    }
+}
