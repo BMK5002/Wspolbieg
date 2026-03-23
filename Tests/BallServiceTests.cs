4fb8778d41ad6b395fdebfbaf9c7fd20ab380272
BMK5002 <tadzio2005@gmail.com>
2026-03-23 21:27:40 +0100

    Added namespace to tests

--- Tests/BallServiceTests.cs
+++ Tests/BallServiceTests.cs
@@ -1,17 +1,20 @@
 ﻿using Data;
 using Model;
 
-public class BallServiceTests
+namespace BallServiceTests
 {
-    [Fact]
-    public void BallMoveTest()
+    public class BallServiceTests
     {
-        var service = new BallService();
-        var Ball = new Ball { X = 10, Y = 10, VelocityX = 1, VelocityY = 1 };
+        [Fact]
+        public void BallMoveTest()
+        {
+            var service = new BallService();
+            var Ball = new Ball { X = 10, Y = 10, VelocityX = 1, VelocityY = 1 };
 
-        service.Update(Ball, 100, 100);
+            service.Update(Ball, 100, 100);
 
-        Assert.Equal(11, Ball.X);
-        Assert.Equal(11, Ball.Y);
+            Assert.Equal(11, Ball.X);
+            Assert.Equal(11, Ball.Y);
+        }
     }
 }
--- Tests/BallTests.cs
+++ Tests/BallTests.cs
@@ -1,37 +1,38 @@
 ﻿using Data;
-using Xunit;
 
-public class BallTests
-{
-    [Fact]
-    public void BallValuesTest()
+namespace BallTests {
+    public class BallTests
     {
-        var Ball = new Ball
+        [Fact]
+        public void BallValuesTest()
         {
-            X = 10,
-            Y = 20,
-            VelocityX = 1.5,
-            VelocityY = -2.5
-        };
+            var Ball = new Ball
+            {
+                X = 10,
+                Y = 20,
+                VelocityX = 1.5,
+                VelocityY = -2.5
+            };
 
-        Assert.Equal(10, Ball.X);
-        Assert.Equal(20, Ball.Y);
-        Assert.Equal(1.5, Ball.VelocityX);
-        Assert.Equal(-2.5, Ball.VelocityY);
-    }
+            Assert.Equal(10, Ball.X);
+            Assert.Equal(20, Ball.Y);
+            Assert.Equal(1.5, Ball.VelocityX);
+            Assert.Equal(-2.5, Ball.VelocityY);
+        }
 
-    [Fact]
-    public void BallSetterTest()
-    {
-        var Ball = new Ball();
+        [Fact]
+        public void BallSetterTest()
+        {
+            var Ball = new Ball();
 
-        Ball.X = 5;
-        Ball.Y = 6;
+            Ball.X = 5;
+            Ball.Y = 6;
 
-        Ball.X = 15;
-        Ball.Y = 16;
+            Ball.X = 15;
+            Ball.Y = 16;
 
-        Assert.Equal(15, Ball.X);
-        Assert.Equal(16, Ball.Y);
+            Assert.Equal(15, Ball.X);
+            Assert.Equal(16, Ball.Y);
+        }
     }
 }
