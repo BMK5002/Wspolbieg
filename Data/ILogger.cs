e6c607eb3ac7fcdc32b91d442c511f543fd6e185
BMK5002 <tadzio2005@gmail.com>
2026-06-08 18:32:07 +0200

    Moved Diagnostics layer to Data layer

---
+++ Data/ILogger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Diagnostics
+{
+    public interface ILogger
+    {
+        void Log(DiagnosticsEntry entry);
+        Task StartAsync();
+        Task StopAsync();
+    }
+}
